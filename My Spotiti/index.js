4216ca5e1c3fa11682b9208c3265539714fe1415
ReygaElkigia <106369845+ReygaElkigia@users.noreply.github.com>
2023-06-05 22:21:18 +0700

    menambahkan file admin+js

--- My Spotiti/index.js
+++ My Spotiti/index.js
@@ -4,7 +4,7 @@ import  Express from 'express'
 const port = 8080;
 const app = Express();
 app.listen(port, ()=>{
-    console.log(`gasta bau memek ${port} is ready`)
+    console.log(`Port ${port} is ready`)
 })
 
 app.set('view engine', 'ejs')
@@ -13,7 +13,12 @@ app.use(Express.static(path.resolve('public')));
 app.get('/login',async (req,res) =>{
     res.render('login')
 });
-
+app.get('/signup',async (req,res) =>{
+    res.render('signup')
+});
 app.get('/HomePage',async (req,res) =>{
     res.render('HomePage')
+});
+app.get('/homepage-admin',async (req,res) =>{
+    res.render('homepage-admin')
 });
